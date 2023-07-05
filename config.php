9194c679d4e7c21474ea9bf2ca7335cfcc2fc00c
KristinaAbrosimova <124628897+KristinaAbrosimova@users.noreply.github.com>
2023-07-05 22:41:55 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$DB = array(
+    "login" => "u0941340_akb",
+    "password" => "qW9lL8xA7slP4jE5",
+    "host" => "31.31.198.115",
+    "name" => "u0941340_akb",
+    "charset" => "utf8mb4"
+);
+
+define("TIME_ZONE", "AsiaTashkent");
